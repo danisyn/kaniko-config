d8de6d337cddec2108ad6831b47bc35c5b2be058
danisyn <daniel.peris@syndeno.com>
2022-05-17 10:33:22 +0200

    updates

---
+++ images/Postfix/start.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Wait before postfix is really started.
+
+function get_state {
+    echo $(script -c 'postfix status' | grep postfix/postfix-script)
+}
+
+postfix start
+echo $(get_state)
+
+while true; do
+    state=$(get_state)
+    if [[ "$state" != "${state/is running/}" ]]; then
+        PID=${state//[^0-9]/}
+        if [[ -z $PID ]]; then
+            continue
+        fi
+        if [[ ! -d "/proc/$PID" ]]; then
+            echo "Postfix proces $PID does not exist."
+            break
+        fi
+    else
+        echo "Postfix is not running."
+        break
+    fi
+done
